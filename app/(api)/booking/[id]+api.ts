ae53e7335138822a7014aba739cd380f7670fbd3
yassine <belgacemyassin46@gmail.com>
2025-01-19 22:45:25 +0100

    add admin approval

--- app/(api)/booking/[id]+api.ts
+++ app/(api)/booking/[id]+api.ts
@@ -1,4 +1,3 @@
-// pages/api/book-borrowing/[id]+api.ts
 import { neon } from "@neondatabase/serverless";
 
 export async function GET(request: Request, { id }: { id: string }) {
@@ -13,6 +12,42 @@ export async function GET(request: Request, { id }: { id: string }) {
     return Response.json({ data: response });
   } catch (error) {
     console.error("Error fetching book borrowing entries:", error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export async function PUT(request: Request, { id }: { id: string }) {
+  if (!id) {
+    return Response.json({ error: "Missing required fields" }, { status: 400 });
+  }
+
+  try {
+    const { status } = await request.json();
+    if (!status || (status !== "Borrowed" && status !== "Refused")) {
+      return Response.json(
+        { error: "Invalid status. Must be 'Approved' or 'Refused'" },
+        { status: 400 },
+      );
+    }
+
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const response = await sql`
+      UPDATE book_borrowing
+      SET status = ${status}
+      WHERE id = ${id} AND status = 'Pending'
+      RETURNING *;
+    `;
+
+    if (response.length === 0) {
+      return Response.json(
+        { error: "Booking not found or already processed" },
+        { status: 404 },
+      );
+    }
+
+    return Response.json({ data: response[0] });
+  } catch (error) {
+    console.error("Error approving or refusing booking:", error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
